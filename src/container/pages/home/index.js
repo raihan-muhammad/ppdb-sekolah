c7123e8b79b58903b7224f82a79d3978a6d4368d
sukasukangoding <sukasukangoding@gmail.com>
2020-05-14 14:28:09 +0700

    init react

---
+++ src/container/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coba</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
