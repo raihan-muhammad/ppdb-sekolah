8318f3f0e555685e0970ea43de9be3f7c842b49b
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-06-14 17:43:19 +0700

    Updates

---
+++ precache-manifest.d2fb0d8eaaecc40977938723c10806bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd71788e55c41347bedf58f3a406c7f9",
+    "url": "/ppdb-sekolah/index.html"
+  },
+  {
+    "revision": "c87fba7f89c678527538",
+    "url": "/ppdb-sekolah/static/css/main.bd89a6c1.chunk.css"
+  },
+  {
+    "revision": "0edb81a2424b959bca06",
+    "url": "/ppdb-sekolah/static/js/2.2d2699c6.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/ppdb-sekolah/static/js/2.2d2699c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87fba7f89c678527538",
+    "url": "/ppdb-sekolah/static/js/main.1816696c.chunk.js"
+  },
+  {
+    "revision": "0266bd4796566e856d36",
+    "url": "/ppdb-sekolah/static/js/runtime-main.8d42194d.js"
+  },
+  {
+    "revision": "bed7a00a9723923c9954237e60abb83c",
+    "url": "/ppdb-sekolah/static/media/IllustratorBiodata.bed7a00a.svg"
+  },
+  {
+    "revision": "5b61a38a4f77f4a5f31746e18a542812",
+    "url": "/ppdb-sekolah/static/media/IllustratorFront.5b61a38a.png"
+  },
+  {
+    "revision": "4fa7391951505920e69020e8f0389527",
+    "url": "/ppdb-sekolah/static/media/IllustratorRegister.4fa73919.svg"
+  },
+  {
+    "revision": "c9c0339e7d65773cd281c4e84b7977f8",
+    "url": "/ppdb-sekolah/static/media/IlustratorSeleksi.c9c0339e.svg"
+  },
+  {
+    "revision": "8fd9e08e735faf0bbd358332523b7694",
+    "url": "/ppdb-sekolah/static/media/header.8fd9e08e.svg"
+  },
+  {
+    "revision": "d2ea2d356a3f8ca6ddf03eb431dc1ea1",
+    "url": "/ppdb-sekolah/static/media/hero_illustrator.d2ea2d35.svg"
+  },
+  {
+    "revision": "8898db4f3cbbb022e7e4010318184b24",
+    "url": "/ppdb-sekolah/static/media/png-elementary-school-primary-education-high-school-school-class-logo-high-school-elementary-school-clipart.8898db4f.png"
+  },
+  {
+    "revision": "d5b4f14dcbc67ec17e0691dab62f18ef",
+    "url": "/ppdb-sekolah/static/media/user_group_two_color.d5b4f14d.svg"
+  }
+]);
